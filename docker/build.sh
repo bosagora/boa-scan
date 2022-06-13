445b82cd15c32d10b5a90eb47a91b844fa1817d2
Chris Hewison <hewisonchris@gmail.com>
2022-06-13 14:53:21 +0900

    Change COIN to BOA

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,7 @@ set -x
 
 REPO=bosagora
 RELEASE_VERSION=agora_v4.1.3    # Update this if it is rebased on a later upstream tag
+COIN=BOA make
 RELEASE_VERSION=$RELEASE_VERSION make tag
 RELEASE_VERSION=$RELEASE_VERSION make tag-stable
 docker push bosagora/blockscout:$RELEASE_VERSION
